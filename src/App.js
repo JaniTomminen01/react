a9437256577471a9daddb73d007c26418f48846c
JaniTomminen01 <111896153+JaniTomminen01@users.noreply.github.com>
2022-09-05 17:54:06 +0300

    tehtävä valmis

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [pituus, setPituus] = useState(1.85)
+  const [paino, setPaino] = useState(60)
+  const [bmi, setBmi] = useState(0)
+
+  const laske = (e) => {
+    e.preventDefault()
+    const tulos = paino / (pituus * pituus)
+    setBmi(tulos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div classname="tausta">
+    <h1>BMI</h1>
+    <form onSubmit={laske}>
+      <div>
+        <label>Pituus</label>
+        <input type="number" value={pituus} onChange={e => setPituus(e.target.value)} />
+
+      </div>
+      <div>
+        <label>Paino</label>
+        <input type="number" value={paino} onChange={e => setPaino(e.target.value)} />
+        
+      </div>
+      <div>
+        <label>BMI</label>
+        <output>{bmi.toFixed(1)}</output>
+      </div>
+      <button>Laske</button>
+    </form>
+   </div>
   );
 }
 
